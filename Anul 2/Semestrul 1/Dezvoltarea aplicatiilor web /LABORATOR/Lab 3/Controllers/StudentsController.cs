1f0b125f6bb689a8c07319a5e63839058cacb27c
Chirițoiu Andra <chiritoiu.andra@yahoo.com>
2025-10-22 00:44:12 +0300

    Add files via upload

---
+++ Anul 2/Semestrul 1/Dezvoltarea aplicatiilor web /LABORATOR/Lab 3/Controllers/StudentsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab3.Controllers
+{
+    public class StudentsController : Controller
+    {
+       public string Index()
+        {
+            return "Afisarea tuturor studentilor";
+        }
+
+        public string Create()
+        {
+            return "Crearea unui nou student";
+        }
+
+        public string Show(int? id) 
+        {
+            if (id is null)
+                return "Studentul nu exista in baza de date";
+            return "Afisare student cu id-ul: " + id;
+        }
+
+        public string Edit(int? id)
+        {
+            if (id is null)
+                return "Studentul nu exista in baza de date";
+            return "Editare student cu id-ul: " + id;
+        }
+
+
+        public string Delete(int? id)
+        {
+            if (id is null)
+                return "Studentul nu exista in baza de date";
+            return "Stergere student cu id-ul: " + id;
+        }
+    }
+}
