d06343b32ad3f030fad6a9bbdbc6472903a90769
Chirițoiu Andra <chiritoiu.andra@yahoo.com>
2025-10-28 01:24:23 +0200

    Add files via upload

---
+++ Anul 2/Semestrul 1/Dezvoltarea aplicatiilor web /LABORATOR/Lab2/Palindrom.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Palindrom
+{
+    int n;
+  
+    public Palindrom(){}
+    public Palindrom(int n)
+    {
+        this.n = n;
+    }
+
+    public bool MetodaVerificarePalindrom(int n)
+    {
+        string s = n.ToString();
+        string invers = new string(s.Reverse().ToArray()); //reverse reuturneaza un iterabil care trebuie transformat in array
+        return s == invers;
+    }
+}
