a500ef58666a20576ec354acceef8cf129cfe880
Chirițoiu Andra <chiritoiu.andra@yahoo.com>
2025-12-03 03:36:50 +0200

    Update lab-console.sql

--- Anul 2/Semestrul 1/Sisteme de gestiune a bazelor de date/LABORATOR/lab-console.sql
+++ Anul 2/Semestrul 1/Sisteme de gestiune a bazelor de date/LABORATOR/lab-console.sql
@@ -942,4 +942,245 @@ END;
 /
 
 --tema e3 si ceva similar cu e3/e4
+--tema
+--E2. Modificați exercițiul anterior astfel încât să obțineți și următoarele informații:
+--un număr de ordine pentru fiecare angajat care va fi resetat pentru fiecare job
+-- pentru fiecare job
+--  o numărul de angajați
+--  o valoarea lunară a veniturilor angajaților
+--  o valoarea medie a veniturilor angajaților
+-- indiferent job
+--  o numărul total de angajați
+--  o valoarea totală lunară a veniturilor angajaților
+--  o valoarea medie a veniturilor angajaților
+
+
+DECLARE
+  CURSOR c_jobs IS
+    SELECT job_id, job_title
+    FROM JOBS;
+
+  CURSOR c_emp(p_job_id VARCHAR2) IS
+    SELECT last_name, salary
+    FROM EMP_CAF
+    WHERE p_job_id=job_id;
+
+    v_nr_crt  NUMBER := 0;
+    v_nr_emp NUMBER := 0;
+    v_suma NUMBER := 0;
+    v_medie NUMBER := 0;
+
+    v_total_emp NUMBER := 0;
+    v_total_suma NUMBER := 0;
+    v_total_medie NUMBER := 0;
+
+BEGIN
+    FOR i IN c_jobs LOOP
+        v_nr_crt :=0;
+        v_nr_emp := 0;
+        v_suma := 0;
+
+        DBMS_OUTPUT.PUT_LINE('--------');
+        DBMS_OUTPUT.PUT_LINE('JOB: ' || i.job_title);
+
+        FOR j in c_emp(i.job_id) LOOP
+            v_nr_crt := v_nr_crt + 1;
+            v_nr_emp := v_nr_emp + 1;
+            v_suma := v_suma + j.salary;
+
+            DBMS_OUTPUT.PUT_LINE(v_nr_crt || '. ' || j.last_name || ', Salariu: ' || j.salary);
+
+            EXIT WHEN c_emp%NOTFOUND;
+        END LOOP;
+
+        v_total_emp := v_total_emp + v_nr_emp;
+        v_total_suma := v_total_suma + v_suma;
+
+        IF v_nr_emp >0 THEN
+             v_medie := ROUND(v_suma/v_nr_emp, 2);
+        DBMS_OUTPUT.PUT_LINE('Statistici Job:');
+        DBMS_OUTPUT.PUT_LINE('Nr. Angajati: ' || v_nr_emp);
+        DBMS_OUTPUT.PUT_LINE('Total Salarii: ' || v_suma);
+        DBMS_OUTPUT.PUT_LINE('Medie Salarii: ' || v_medie);
+
+        ELSE
+            DBMS_OUTPUT.PUT_LINE('Nu exista angajati');
+        END IF;
+
+        EXIT WHEN c_jobs%NOTFOUND;
+    END LOOP;
+
+     IF v_total_emp >0 THEN
+             v_total_medie := ROUND(v_total_suma/v_total_emp, 2);
+        DBMS_OUTPUT.PUT_LINE('--------:');
+        DBMS_OUTPUT.PUT_LINE('Statistici GLOBALE:');
+        DBMS_OUTPUT.PUT_LINE('Nr. Angajati TOTAL: ' || v_total_emp);
+        DBMS_OUTPUT.PUT_LINE('Total Salarii: ' || v_total_suma);
+        DBMS_OUTPUT.PUT_LINE('Medie Salarii TOTAL: ' || v_total_medie);
+
+     ELSE
+            DBMS_OUTPUT.PUT_LINE('Nu exista angajati');
+    END IF;
+
+END;
+
+--E3. Modificați exercițiul anterior astfel încât să obțineți suma totală alocată lunar pentru plata
+-- salariilor și a comisioanelor tuturor angajaților, iar pentru fiecare angajat cât la sută din această
+-- sumă câștigă lunar.(pe ex 10)
+
+--a)cele trei tipuri de cursoare studiate;
+--cursor clasic
+DECLARE
+    CURSOR c_dep IS
+        SELECT department_id, department_name
+        FROM   departments
+        WHERE department_id IN (10,20,30,40);
+
+    CURSOR c_emp(p_dep_id VARCHAR2) IS
+        SELECT last_name, salary, commission_pct
+        FROM EMP_CAF
+        WHERE  department_id = p_dep_id;
+
+    v_dep_id EMP_CAF.department_id%TYPE;
+    v_dep_name  DEPARTMENTS.department_name%TYPE;
+
+    v_emp_name EMP_CAF.last_name%TYPE;
+    v_emp_sal  EMP_CAF.salary%TYPE;
+    v_emp_comm  EMP_CAF.commission_pct%TYPE;
+
+    v_total NUMBER := 0;
+    v_angajat NUMBER := 0;
+    v_procent NUMBER := 0;
+
+
+BEGIN
+    SELECT SUM(salary + (salary * NVL(commission_pct, 0)))
+    INTO v_total
+    FROM EMP_CAF;
+
+    DBMS_OUTPUT.PUT_LINE('Total Salarii + Comisioane in Firma: ' || v_total);
+
+    OPEN c_dep;
+    LOOP
+        FETCH c_dep INTO v_dep_id, v_dep_name;
+        EXIT WHEN c_dep%NOTFOUND;
+
+        DBMS_OUTPUT.PUT_LINE('-------------------------------------');
+        DBMS_OUTPUT.PUT_LINE ('DEPARTAMENT '|| v_dep_name);
+        DBMS_OUTPUT.PUT_LINE('-------------------------------------');
+
+        OPEN c_emp(v_dep_id);
+        LOOP
+            FETCH c_emp INTO v_emp_name, v_emp_sal, v_emp_comm;
+            EXIT WHEN c_emp%NOTFOUND;
+
+            v_angajat := v_emp_sal + (v_emp_sal * NVL(v_emp_comm, 0)) ;
+
+            IF v_total > 0 THEN
+                v_procent := (v_angajat / v_total) * 100;
+            ELSE
+                v_procent := 0;
+            END IF;
+
+            DBMS_OUTPUT.PUT_LINE('Angajat: ' || v_emp_name ||
+                                 ' | Venit: ' || v_angajat ||
+                                 ' | % din Total: ' || ROUND(v_procent, 4) || '%');
+        END LOOP;
+        CLOSE c_emp;
+
+    END LOOP;
+    CLOSE c_dep;
+
+    END;
+/
+
+
+--ciclu cursor
+    DECLARE
+    CURSOR c_dep IS
+        SELECT department_id, department_name
+        FROM   departments
+        WHERE department_id IN (10,20,30,40);
+
+    CURSOR c_emp(p_dep_id VARCHAR2) IS
+        SELECT last_name, salary, commission_pct
+        FROM EMP_CAF
+        WHERE  department_id = p_dep_id;
+
+    v_total NUMBER := 0;
+    v_angajat NUMBER := 0;
+    v_procent NUMBER := 0;
+
+
+BEGIN
+    SELECT SUM(salary + (salary * NVL(commission_pct, 0)))
+    INTO v_total
+    FROM EMP_CAF;
+
+    DBMS_OUTPUT.PUT_LINE('Total Salarii + Comisioane in Firma: ' || v_total);
+
+
+  FOR i in c_dep LOOP
+        EXIT WHEN c_dep%NOTFOUND;
+
+        DBMS_OUTPUT.PUT_LINE('-------------------------------------');
+        DBMS_OUTPUT.PUT_LINE ('DEPARTAMENT '|| i.department_id);
+        DBMS_OUTPUT.PUT_LINE('-------------------------------------');
+
+
+        FOR j IN c_emp(i.department_id) LOOP
+            EXIT WHEN c_emp%NOTFOUND;
+
+            v_angajat := j.salary + (j.salary * NVL(j.commission_pct, 0)) ;
+
+            IF v_total > 0 THEN
+                v_procent := (v_angajat / v_total) * 100;
+            ELSE
+                v_procent := 0;
+            END IF;
+
+            DBMS_OUTPUT.PUT_LINE('Angajat: ' || j.last_name ||
+                                 ' | Venit: ' || v_angajat ||
+                                 ' | % din Total: ' || ROUND(v_procent, 4) || '%');
+        END LOOP;
+
+    END LOOP;
+
+    END;
+/
+
+
+
+-- ciclu cursor cu subcereri
+DECLARE
+    v_total_global NUMBER := 0;
+    v_venit_emp    NUMBER := 0;
+    v_procent      NUMBER(5, 2);
+BEGIN
+    SELECT SUM(salary + (salary * NVL(commission_pct, 0))) INTO v_total_global FROM EMP_CAF;
+    DBMS_OUTPUT.PUT_LINE('TOTAL VENITURI FIRMA: ' || v_total_global);
+
+    FOR r_job IN (SELECT job_id, job_title FROM JOBS ORDER BY job_title) LOOP
+
+        DBMS_OUTPUT.PUT_LINE('JOB: ' || r_job.job_title);
+        DBMS_OUTPUT.PUT_LINE('------------------------------------');
+
+
+        FOR r_emp IN (SELECT last_name, salary, commission_pct
+                      FROM EMP_CAF
+                      WHERE job_id = r_job.job_id
+                      ORDER BY salary DESC) LOOP
+
+            v_venit_emp := r_emp.salary + (r_emp.salary * NVL(r_emp.commission_pct, 0));
+            v_procent := (v_venit_emp / v_total_global) * 100;
+
+            DBMS_OUTPUT.PUT_LINE('   -> ' || RPAD(r_emp.last_name, 15) ||
+                                 ' | Venit: ' || v_venit_emp ||
+                                 ' | Procent: ' || v_procent || '%');
+        END LOOP;
+        DBMS_OUTPUT.PUT_LINE('');
+    END LOOP;
+END;
+/
+
 
