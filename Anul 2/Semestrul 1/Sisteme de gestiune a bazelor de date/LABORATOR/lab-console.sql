ebb4ad2393e6073f51841f9d6030990dcecf85ea
Chirițoiu Andra <chiritoiu.andra@yahoo.com>
2025-12-22 19:38:14 +0200

    Update lab-console.sql

--- Anul 2/Semestrul 1/Sisteme de gestiune a bazelor de date/LABORATOR/lab-console.sql
+++ Anul 2/Semestrul 1/Sisteme de gestiune a bazelor de date/LABORATOR/lab-console.sql
@@ -1183,4 +1183,594 @@ BEGIN
 END;
 /
 
+-- --tema 8
+-- --e1
+CREATE TABLE info_CAF(
+    utilizator VARCHAR2(50),
+    data DATE,
+    comanda VARCHAR2(50),
+    nr_linii NUMBER,
+    eroare VARCHAR(255)
+);
+
+
+--e3
+SELECT *
+FROM EMPLOYEES;
+
+CREATE OR REPLACE FUNCTION fe3_CAF
+    (v_oras VARCHAR2)
+RETURN NUMBER
+IS
+    v_numar_angajati NUMBER := 0;
+    v_numar_locatii NUMBER := 0;
+    v_numar_joburi NUMBER := 0;
+
+BEGIN
+    SELECT COUNT(*)
+    INTO v_numar_locatii
+    FROM LOCATIONS
+    WHERE UPPER(city) = UPPER(v_oras);
+
+    IF v_numar_locatii = 0 THEN
+        INSERT INTO info_CAF (utilizator, data, comanda, nr_linii, eroare)
+        VALUES (USER, SYSDATE, 'fe3_CAF', 0, 'EROARE: Orasul ' || v_oras || ' nu exista in baza de date.');
+        COMMIT;
+        RETURN 0;
+
+    END IF ;
+
+    SELECT COUNT(DISTINCT e.employee_id)
+    INTO v_numar_angajati
+    FROM EMPLOYEES e
+    JOIN DEPARTMENTS d ON d.department_id=e.department_id
+    JOIN LOCATIONS L ON l.location_id=d.location_id
+    WHERE UPPER(l.city) = UPPER(v_oras) AND
+          e.employee_id IN (
+                SELECT employee_id
+                FROM JOB_HISTORY
+                GROUP BY employee_id
+                HAVING COUNT(DISTINCT job_id) >= 2
+              );
+
+    IF v_numar_angajati = 0 THEN
+        INSERT INTO info_CAF (utilizator, data, comanda, nr_linii, eroare)
+        VALUES (USER, SYSDATE, 'fe3_CAF', 0, 'ZERO: Orasul exista, dar niciun angajat nu indeplineste criteriile.');
+    ELSE
+        INSERT INTO info_CAF (utilizator, data, comanda, nr_linii, eroare)
+        VALUES (USER, SYSDATE, 'fe3_CAF', v_numar_angajati , 'Au fost gasiti ' || v_numar_angajati  || ' angajati.');
+    END IF;
+
+    COMMIT;
+    RETURN v_numar_angajati;
+
+END fe3_CAF;
+
+DECLARE
+    v_rezultat NUMBER;
+    v_oras_cautat VARCHAR2(50) := 'Seattle';
+BEGIN
+    v_rezultat := fe3_CAF(v_oras_cautat);
+
+    DBMS_OUTPUT.PUT_LINE('Oras cautat: ' || v_oras_cautat);
+    DBMS_OUTPUT.PUT_LINE('Numar angajati gasiti: ' || v_rezultat);
+
+END;
+
+SELECT *
+FROM info_CAF;
+
+
+--e4
+CREATE OR REPLACE PROCEDURE fe4_CAF
+    (v_id_manager EMPLOYEES.manager_id%TYPE)
+IS
+    v_nr_manageri NUMBER := 0;
+    v_randuri_modif NUMBER := 0;
+
+BEGIN
+    SELECT COUNT(*)
+    INTO v_nr_manageri
+    FROM EMPLOYEES
+    WHERE manager_id = v_id_manager;
+
+    IF v_nr_manageri = 0 THEN
+        INSERT INTO info_CAF (utilizator, data, comanda, nr_linii, eroare)
+        VALUES (USER, SYSDATE, 'fe4_CAF', 0, 'EROARE: Managerul cu ID ' || v_id_manager || ' nu exista.');
+        COMMIT;
+    END IF;
+
+    UPDATE EMP_CAF e
+    SET e.salary=e.salary+0.10*e.salary
+    WHERE e.employee_id IN(
+        SELECT employee_id
+        FROM EMPLOYEES
+        START WITH manager_id = v_id_manager
+        CONNECT BY PRIOR employee_id = manager_id
+        );
+
+    v_randuri_modif := SQL%ROWCOUNT;
+
+    IF v_randuri_modif = 0 THEN
+        INSERT INTO info_CAF (utilizator, data, comanda, nr_linii, eroare)
+        VALUES (USER, SYSDATE, 'fe4_CAF', 0, 'ZERO: Managerul ' || v_id_manager || ' nu are subordonati (salariu nemodificat).');
+    ELSE
+        INSERT INTO info_CAF (utilizator, data, comanda, nr_linii, eroare)
+        VALUES (USER, SYSDATE, 'fe4_CAF', v_randuri_modif, 'SUCCES: S-au marit salariile pentru ' || v_randuri_modif || ' subordonati.');
+    END IF;
+
+    COMMIT;
+
+END fe4_CAF;
+
+
+
+SELECT *
+FROM EMP_CAF;
+
+
+
+DECLARE
+    v_manager EMPLOYEES.manager_id%TYPE := 100;
+BEGIN
+    FE4_CAF(v_manager);
+    DBMS_OUTPUT.PUT_LINE('Procedura executata cu succes pentru managerul: ' || v_manager);
+END;
+
+SELECT *
+FROM EMP_CAF;
+
+
+
+
+--tema 9
+--E1
+CREATE OR REPLACE PACKAGE pachet_angajati_CAF AS
+    --f
+    CURSOR c_angajati_job(p_job JOBS.job_id%TYPE)
+        RETURN EMPLOYEES%ROWTYPE IS
+    SELECT *
+    FROM EMPLOYEES
+    WHERE job_id = p_job;
+
+    --g
+    CURSOR c_joburi
+        RETURN JOBS%ROWTYPE IS
+    SELECT *
+    FROM JOBS;
+
+
+    --a
+    FUNCTION f_sal_min(v_dep_id  DEPARTMENTS.department_id%TYPE, v_job JOBS.job_id%TYPE)
+        RETURN EMPLOYEES.salary%TYPE;
+
+    FUNCTION f_id_manager(v_nume EMPLOYEES.last_name%TYPE, v_prenume EMPLOYEES.first_name%TYPE)
+        RETURN EMPLOYEES.employee_id%TYPE;
+
+    FUNCTION f_id_dep(v_nume_dep DEPARTMENTS.department_name%TYPE)
+        RETURN DEPARTMENTS.department_id%TYPE;
+
+    FUNCTION f_job_id(v_nume_job JOBS.job_title%TYPE)
+        RETURN JOBS.job_id%TYPE;
+
+    PROCEDURE adauga_angajat(
+        p_nume EMPLOYEES.last_name%TYPE,
+        p_prenume EMPLOYEES.first_name%TYPE,
+        p_telefon EMPLOYEES.phone_number%TYPE,
+        p_email EMPLOYEES.email%TYPE,
+        p_nume_mgr EMPLOYEES.last_name%TYPE,
+        p_prenume_mgr EMPLOYEES.first_name%TYPE,
+        p_dep_name DEPARTMENTS.department_name%TYPE,
+        p_job_name JOBS.job_title%TYPE
+    );
+
+    --b
+    PROCEDURE muta_angajat(
+        p_nume EMPLOYEES.last_name%TYPE,
+        p_prenume EMPLOYEES.first_name%TYPE,
+        p_dep_nou DEPARTMENTS.department_name%TYPE,
+        p_job_nou JOBS.job_title%TYPE,
+        p_nume_mgr EMPLOYEES.last_name%TYPE,
+        p_prenume_mgr EMPLOYEES.first_name%TYPE
+    );
+
+
+    --e
+    PROCEDURE update_salariu(
+        p_nume EMPLOYEES.last_name%TYPE,
+        p_salariu NUMBER
+    );
+
+END pachet_angajati_CAF;
+/
+
+
+CREATE OR REPLACE PACKAGE BODY pachet_angajati_CAF AS
+    --a
+     FUNCTION f_sal_min(v_dep_id  DEPARTMENTS.department_id%TYPE, v_job JOBS.job_id%TYPE)
+     RETURN EMPLOYEES.salary%TYPE IS
+         v_sal EMPLOYEES.salary%TYPE;
+     BEGIN
+         SELECT MIN(salary)
+         INTO v_sal
+         FROM EMPLOYEES
+         WHERE v_dep_id = department_id
+            AND v_job = job_id;
+         RETURN v_sal;
+    END;
+
+    FUNCTION f_id_manager(v_nume EMPLOYEES.last_name%TYPE, v_prenume EMPLOYEES.first_name%TYPE)
+    RETURN EMPLOYEES.employee_id%TYPE IS
+        v_id EMPLOYEES.employee_id%TYPE;
+    BEGIN
+        SELECT employee_id
+        INTO v_id
+        FROM EMPLOYEES
+        WHERE last_name = v_nume
+        AND first_name = v_prenume;
+
+    RETURN v_id;
+    END;
+
+    FUNCTION f_id_dep(v_nume_dep DEPARTMENTS.department_name%TYPE)
+    RETURN DEPARTMENTS.department_id%TYPE IS
+        v_id DEPARTMENTS.department_id%TYPE;
+    BEGIN
+        SELECT department_id
+        INTO v_id
+        FROM DEPARTMENTS
+        WHERE department_name = v_nume_dep;
+
+    RETURN v_id;
+    END;
+
+    FUNCTION f_job_id(v_nume_job JOBS.job_title%TYPE)
+    RETURN JOBS.job_id%TYPE IS
+        v_id JOBS.job_id%TYPE;
+    BEGIN
+        SELECT job_id
+        INTO v_id
+        FROM JOBS
+        WHERE job_title = v_nume_job;
+
+    RETURN v_id;
+    END;
+
+    PROCEDURE adauga_angajat(
+    p_nume EMPLOYEES.last_name%TYPE,
+    p_prenume EMPLOYEES.first_name%TYPE,
+    p_telefon EMPLOYEES.phone_number%TYPE,
+    p_email EMPLOYEES.email%TYPE,
+    p_nume_mgr EMPLOYEES.last_name%TYPE,
+    p_prenume_mgr EMPLOYEES.first_name%TYPE,
+    p_dep_name DEPARTMENTS.department_name%TYPE,
+    p_job_name JOBS.job_title%TYPE
+    ) IS
+        v_mgr EMPLOYEES.employee_id%TYPE;
+        v_dep DEPARTMENTS.department_id%TYPE;
+        v_job JOBS.job_id%TYPE;
+        v_sal EMPLOYEES.salary%TYPE;
+    BEGIN
+        v_mgr := f_id_manager(p_nume_mgr, p_prenume_mgr);
+        v_dep := f_id_dep(p_dep_name);
+        v_job := f_job_id(p_job_name);
+        v_sal := f_sal_min(v_dep, v_job);
+
+    INSERT INTO EMPLOYEES
+    VALUES (
+        employees_seq.NEXTVAL,
+        p_prenume,
+        p_nume,
+        p_email,
+        p_telefon,
+        SYSDATE,
+        v_job,
+        v_sal,
+        NULL,
+        v_mgr,
+        v_dep
+    );
+    END;
+
+    --b
+    PROCEDURE muta_angajat(
+        p_nume EMPLOYEES.last_name%TYPE,
+        p_prenume EMPLOYEES.first_name%TYPE,
+        p_dep_nou DEPARTMENTS.department_name%TYPE,
+        p_job_nou JOBS.job_title%TYPE,
+        p_nume_mgr EMPLOYEES.last_name%TYPE,
+        p_prenume_mgr EMPLOYEES.first_name%TYPE
+    ) IS
+        v_id EMPLOYEES.employee_id%TYPE;
+        v_dep DEPARTMENTS.department_id%TYPE;
+        v_job JOBS.job_id%TYPE;
+        v_mgr EMPLOYEES.employee_id%TYPE;
+        v_sal_cur EMPLOYEES.salary%TYPE;
+        v_sal_min EMPLOYEES.salary%TYPE;
+    BEGIN
+        SELECT employee_id, salary
+        INTO v_id, v_sal_cur
+        FROM EMPLOYEES
+        WHERE last_name = p_nume
+          AND first_name = p_prenume;
+
+        v_dep := f_id_dep(p_dep_nou);
+        v_job := f_job_id(p_job_nou);
+        v_mgr := f_id_manager(p_nume_mgr, p_prenume_mgr);
+        v_sal_min := f_sal_min(v_dep, v_job);
+
+        INSERT INTO JOB_HISTORY
+        VALUES (v_id, SYSDATE, SYSDATE, v_job, v_dep);
+
+        UPDATE EMPLOYEES
+        SET department_id = v_dep,
+            job_id = v_job,
+            manager_id = v_mgr,
+            salary = GREATEST(v_sal_cur, v_sal_min),
+            hire_date = SYSDATE
+        WHERE employee_id = v_id;
+    END;
+
+    --e
+    PROCEDURE update_salariu(
+        p_nume EMPLOYEES.last_name%TYPE,
+        p_salariu NUMBER
+    ) IS
+        v_count NUMBER;
+        v_job JOBS.job_id%TYPE;
+        v_min JOBS.min_salary%TYPE;
+        v_max JOBS.max_salary%TYPE;
+    BEGIN
+        SELECT COUNT(*)
+        INTO v_count
+        FROM EMPLOYEES
+        WHERE last_name = p_nume;
+
+        IF v_count = 0 THEN
+            DBMS_OUTPUT.PUT_LINE('Nu exista angajat cu acest nume');
+
+        ELSIF v_count > 1 THEN
+            DBMS_OUTPUT.PUT_LINE('Exista mai multi angajati cu acest nume');
+            FOR e IN (SELECT first_name, salary
+                      FROM EMPLOYEES
+                      WHERE last_name = p_nume) LOOP
+                DBMS_OUTPUT.PUT_LINE(e.first_name || ' ' || e.salary);
+            END LOOP;
+
+        ELSE
+            SELECT job_id
+            INTO v_job
+            FROM EMPLOYEES
+            WHERE last_name = p_nume;
+
+            SELECT min_salary, max_salary
+            INTO v_min, v_max
+            FROM JOBS
+            WHERE job_id = v_job;
+
+            IF p_salariu BETWEEN v_min AND v_max THEN
+                UPDATE EMPLOYEES
+                SET salary = p_salariu
+                WHERE last_name = p_nume;
+            ELSE
+                DBMS_OUTPUT.PUT_LINE('Salariul nu respecta limitele jobului');
+            END IF;
+        END IF;
+    END;
+
+END pachet_angajati_CAF;
+
+
+
+--a
+BEGIN
+  pachet_angajati_CAF.adauga_angajat(
+    p_nume        => 'Popescu',
+    p_prenume     => 'Ion',
+    p_telefon     => '0712345678',
+    p_email       => 'IPOPESCU',
+    p_nume_mgr    => 'King',
+    p_prenume_mgr => 'Steven',
+    p_dep_name    => 'Sales',
+    p_job_name    => 'Sales Representative'
+  );
+END;
+/
+
+SELECT last_name, department_id, job_id, salary
+FROM EMPLOYEES
+WHERE last_name = 'Popescu';
+
+
+--b
+BEGIN
+  pachet_angajati_CAF.muta_angajat(
+    p_nume        => 'Popescu',
+    p_prenume     => 'Ion',
+    p_dep_nou     => 'IT',
+    p_job_nou     => 'Programmer',
+    p_nume_mgr    => 'Hunold',
+    p_prenume_mgr => 'Alexander'
+  );
+END;
+/
+SELECT department_id, job_id, manager_id, salary
+FROM EMPLOYEES
+WHERE last_name = 'Popescu';
+
+
+--e
+BEGIN
+  pachet_angajati_CAF.update_salariu('Popescu', 9000);
+END;
+/
+SELECT last_name, salary
+FROM EMPLOYEES
+WHERE last_name = 'Popescu';
+
+
+--f
+DECLARE
+    v_emp EMPLOYEES%ROWTYPE;
+BEGIN
+    OPEN pachet_angajati_CAF.c_angajati_job('IT_PROG');
+
+    LOOP
+        FETCH pachet_angajati_CAF.c_angajati_job INTO v_emp;
+        EXIT WHEN pachet_angajati_CAF.c_angajati_job%NOTFOUND;
+
+        DBMS_OUTPUT.PUT_LINE(
+            v_emp.last_name || ' ' ||
+            v_emp.first_name || ' | Salariu: ' ||
+            v_emp.salary
+        );
+    END LOOP;
+
+    CLOSE pachet_angajati_CAF.c_angajati_job;
+END;
+/
+
+
+
+--g
+DECLARE
+  v_job JOBS%ROWTYPE;
+BEGIN
+  OPEN pachet_angajati_CAF.c_joburi;
+  LOOP
+    FETCH pachet_angajati_CAF.c_joburi INTO v_job;
+    EXIT WHEN pachet_angajati_CAF.c_joburi%NOTFOUND;
+    DBMS_OUTPUT.PUT_LINE(v_job.job_id || ' - ' || v_job.job_title);
+  END LOOP;
+  CLOSE pachet_angajati_CAF.c_joburi;
+END;
+/
+
+
+--lab10
+--1
+CREATE OR REPLACE TRIGGER trig_insert_restrict
+BEFORE INSERT ON EMP_CAF
+DECLARE
+BEGIN
+
+    IF TO_CHAR(SYSDATE, 'HH24') > 18 THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Inserarea de noi angajati nu este permisa dupa ora 18:00.');
+    END IF;
+END;
+/
+
+--2
+CREATE OR REPLACE TRIGGER trig_comm_not_negative
+BEFORE UPDATE OF commission_pct ON EMP_CAF
+FOR EACH ROW
+BEGIN
+     IF :NEW.commission_pct < 0 THEN
+        RAISE_APPLICATION_ERROR(-20005, 'Valoarea comisionului (commission_pct) nu poate fi negativa.');
+    END IF;
+END;
+/
+
+--3
+CREATE OR REPLACE TRIGGER trig_check_salary_limits
+BEFORE INSERT OR UPDATE OF salary ON EMP_cAF
+FOR EACH ROW
+DECLARE
+    v_min_sal jobs.min_salary%TYPE;
+    v_max_sal jobs.max_salary%TYPE;
+BEGIN
+    SELECT min_salary, max_salary
+    INTO v_min_sal, v_max_sal
+    FROM jobs
+    WHERE job_id = :NEW.job_id;
+
+    IF :NEW.salary < v_min_sal THEN
+        RAISE_APPLICATION_ERROR(-20007,
+            'Salariul (' || :NEW.salary || ') este sub limita minima (' || v_min_sal || ') pentru job-ul ' || :NEW.job_id);
+    END IF;
+
+    IF :NEW.salary > v_max_sal THEN
+        RAISE_APPLICATION_ERROR(-20008,
+            'Salariul (' || :NEW.salary || ') depaseste limita maxima (' || v_max_sal || ') pentru job-ul ' || :NEW.job_id);
+    END IF;
+
+END;
+/
+
+
+
+--tema
+--e2,e3, din lab trecut
+
+
+
+--lab11
+--e4
+
+
+CREATE OR REPLACE TRIGGER trig_e4_CAF
+    AFTER INSERT OR UPDATE OF department_id ON EMP_CAF
+DECLARE
+    CURSOR c_verificare IS
+        SELECT department_id, COUNT(*) as nr_angajati
+        FROM EMP_CAF
+        GROUP BY department_id
+        HAVING COUNT(*) > 45;
+BEGIN
+    FOR r IN c_verificare LOOP
+        RAISE_APPLICATION_ERROR(-20001,
+            'Eroare: Departamentul cu ID-ul ' || r.department_id ||
+            ' a depășit limita de 45 de angajați. (Total curent: ' || r.nr_angajati || ')');
+    END LOOP;
+END;
+/
+
+UPDATE EMP_CAF
+SET department_id = 50
+WHERE 1=1;
+
+
+
+--E5
+create table emp_test_CAF
+    as select employee_id, first_name, last_name, department_id from employees;
+alter table emp_test_CAF
+    add constraint pk_emp_test_ari primary key (employee_id);
+
+create table dept_test_CAF
+    as select department_id, department_name from departments;
+alter table dept_test_CAF
+    add constraint pk_dept_test_ari primary key (department_id);
+
+
+
+CREATE OR REPLACE TRIGGER trg_cascada_CAF
+    AFTER DELETE OR UPDATE OF department_id ON dept_test_CAF
+    FOR EACH ROW
+BEGIN
+    IF DELETING THEN
+        DELETE FROM emp_test_CAF
+        WHERE department_id = :OLD.department_id;
+
+    ELSIF UPDATING THEN
+        UPDATE emp_test_CAF
+        SET department_id = :NEW.department_id
+        WHERE department_id = :OLD.department_id;
+    END IF;
+END;
+/
+
+--VERIF
+SELECT * FROM emp_test_CAF WHERE department_id = 10;
+
+UPDATE dept_test_CAF
+SET department_id = 15
+WHERE department_id = 10;
+
+SELECT * FROM emp_test_CAF WHERE department_id = 15;
+
+
+DELETE FROM dept_test_CAF WHERE department_id = 20;
+
+SELECT * FROM emp_test_CAF WHERE department_id = 20;
 
